1cf45681379b8d480c703b0d0ae07000f1bac34a
Yu Jing <yujing5b5d@gmail.com>
2015-04-25 17:05:35 +0800

    syslog

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
 // Copyright 2014 Yu Jing<yujing5b5d@gmail.com>
 #include <cstdio>
 
+#include <syslog.h> // syslog
+
 #include <vector>
 #include <string>
 
@@ -13,6 +15,7 @@
 #include "argcv/string/string.hh"
 #include "argcv/string/hash.hh"
 #include "argcv/string/uuid.hh"
+#include "argcv/sys/daemon.h"
 #include "argcv/timer/timer.hh"
 
 using argcv::argcv_info;
@@ -27,8 +30,12 @@ using namespace argcv::string;
 using namespace argcv::net;
 
 int main(int argc, char* argv[]) {
-    argcv::timer::timer t;
-    printf("argcv_seed .... \n");
-    printf("up to : %f ms\n", t.all());
+    //argcv::timer::timer t;
+    //printf("argcv_seed .... \n");
+    //printf("up to : %f ms\n", t.all());
+    memcpy(proj_work_dir,"/tmp",5);
+    daemon_init("argcv_seed");
+    syslog(LOG_INFO,"hello .. said by argcv_seed ... \n");
+    daemon_destroy();
     return 0;
 }
