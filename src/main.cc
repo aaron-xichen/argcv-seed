46804589a9dea594039abfc3f8a36d5b5d1acfae
Yu Jing <yujing5b5d@gmail.com>
2015-04-25 16:19:00 +0800

    update argcv , colacus & timer

--- src/main.cc
+++ src/main.cc
@@ -8,10 +8,12 @@
 #include "argcv/alg/alg.hh"
 #include "argcv/ml/corpus/corpus.hh"
 #include "argcv/ml/corpus/ds.hh"
+#include "argcv/net/co_lacus.hh"
 #include "argcv/random/random.hh"
 #include "argcv/string/string.hh"
 #include "argcv/string/hash.hh"
 #include "argcv/string/uuid.hh"
+#include "argcv/timer/timer.hh"
 
 using argcv::argcv_info;
 
@@ -22,39 +24,11 @@ using argcv::ml::corpus::dataset_d;
 
 using namespace argcv::string;
 
-int main(int argc, char * argv[]) {
-    uuid u1(0x33);
-    uuid u2(0x33);
-    for(int i = 0 ; i < 8; i ++ ) {
-        printf("%c",u1.hex()[i]);
-    }
-    printf("-");
-    for(int i = 8 ; i < 12; i ++ ) {
-        printf("%c",u1.hex()[i]);
-    }
-    printf("-");
-    for(int i = 12; i < 16; i ++ ) {
-        printf("%c",u1.hex()[i]);
-    }
-    printf("-");
-    for(int i = 16; i < 20; i ++ ) {
-        printf("%c",u1.hex()[i]);
-    }
-    printf("-");
-    for(int i = 20; i < 32; i ++ ) {
-        printf("%c",u1.hex()[i]);
-    }
-    printf("\n");
-    std::pair<uint64_t,uint64_t> p = u1.pair();
-    printf("%s # %llx %llx\n",u1.hex().c_str(),p.first,p.second);
-    
-    p = u2.pair();
-    printf("%s # %llx %llx\n",u2.hex().c_str(),p.first,p.second);
-    
+using namespace argcv::net;
+
+int main(int argc, char* argv[]) {
+    argcv::timer::timer t;
+    printf("argcv_seed .... \n");
+    printf("up to : %f ms\n", t.all());
     return 0;
 }
-
-
-
-
-
